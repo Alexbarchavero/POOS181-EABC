dfe8c342166730731a8dc90bf1a90cc43e7fe83f
Alexbarchavero <125913095+Alexbarchavero@users.noreply.github.com>
2023-07-03 17:13:07 -0600

    PRACTICA 8: PREPARACION DE VISTA EDITAR CON LOS INPUTS PRECARGADOS

--- flask/app.py
+++ flask/app.py
@@ -1,44 +1,61 @@
-#Importacion del framework
+#Importar el framework
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 #Inicializacion del APP
 app = Flask(__name__)
+
+#Configuracion de la conexion a la BD
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'dbflask'
+app.config['MYSQL_DB'] ='dbflask'
 app.secret_key = 'mysecretkey'
-
 mysql = MySQL(app)
 
-#Declaracion de ruta http://localhost:5000
-@app.route('/')
+#Declaracion de rutas http://localhost:5000/
+@app.route("/")
 def index():
-    return render_template("index.html")
+    cc = mysql.connection.cursor()
+    cc.execute('select * from tbAlbums')
+    consultaAlbums = cc.fetchall()
+    #print (consultaAlbums)
+    return render_template('index.html', lsAlbums = consultaAlbums)
 
-#ruta http://localhost:5000/guardar tipo POST para Insert
-@app.route('/guardar', methods=['POST'])
+#Declaracion de rutas http://localhost:5000/guardar tipo post
+@app.route("/guardar", methods=['POST'])
 def guardar():
     if request.method == 'POST':
-        # pasamos a variables el contenido de los input
-        vtitulo = request.form['txtTitulo']
-        vartista = request.form['txtArtista']
-        vanio = request.form['txtAnio']
+        #Pasamos a las variables el contenido de los inputs
+        Vtitulo=request.form['txtTitulo']
+        Vartista=request.form['txtArtista']
+        Vanio=request.form['txtAnio']
         
-        #print(titulo,artista,anio)
-        
-        #Conectar y ejecutar el insert
-        CS = mysql.connection.cursor()
-        CS.execute('INSERT INTO tbAlbums(titulo,artista,anio) VALUES(%s,%s,%s)',(vtitulo,vartista,vanio))
+        #Conectar nuestra BD y ejecutar el insert
+
+        cs = mysql.connection.cursor()
+        cs.execute('insert into tbAlbums (titulo, artista, anio) values (%s, %s, %s)', (Vtitulo, Vartista, Vanio))
         mysql.connection.commit()
-    flash('El album fue agregado correctamente')
+
+    flash('Los datos se guardaron correctamente')
     return redirect(url_for('index'))
 
-@app.route('/eliminar')
-def eliminar():
-    return "Se elimino la BD"
+@app.route('/editar/<id>')
+def editar(id):
+    cursorId = mysql.connection.cursor()
+    cursorId.execute('select * from tbAlbums where id=%s',(id,))
+    consultaId = cursorId.fetchall()
+    return render_template('editarAlbum.html', album = consultaId)
 
-#Ejecucion del servidor en puerto 5000
-if __name__ == "__main__":
-    app.run(port=5000, debug = True)
+@app.route('/actualizar/<id>',methods=['POST'])
+def actualizar(id):
+    
+
+#Declaracion de rutas http://localhost:5000/
+@app.route("/eliminar")
+def eliminar():
+    return "Se eliminó en la BD"
+
+#Ejecucion del Servidor en el puerto 5000
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
